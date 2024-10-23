e72e7405864f9f1ac158707d5947e998ddeefe19
lucaGomezB <gomez.luca.007@gmail.com>
2024-10-23 20:07:30 -0300

    fixing stage 1

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+#Acá se importa lo que usaremos
+
+API_KEY = os.getenv('API_KEY')
+#Con esto accedemos a la API
 
 #Primero se importa moduloFuncionesGraficasBack, que tiene el root.mainloop() : 
 import moduloFuncionesGraficasBack
+
+# Ejecutar el loop principal de Tkinter
+
+display_value = os.environ.get('DISPLAY')
+if (display_value is None): 
+    pass
+else:
+    moduloFuncionesGraficasBack.root.mainloop()
